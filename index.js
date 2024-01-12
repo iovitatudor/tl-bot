0903e7c921ace7a575e43b116985b3fc208fd1fc
Tudor Iovita <iovitatudor21@gmail.com>
2024-01-12 18:32:56 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = "6894605231:AAHhMrGjVjcHCtEdVda9308cvYt6s2sWmvc\n";
+const bot = new TelegramBot(token, {polling: true});
+const timeTG = "https://front.time.cheap";
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const message = msg.text;
+    if (message === "/start") {
+        await bot.sendMessage(chatId, "TimeCoin Lottery", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Go to TimeCoin Lottery', web_app: {url: timeTG}}]
+                ],
+            }
+        })
+
+        await bot.sendMessage(chatId, "/start", {
+            reply_markup: {
+                keyboard: [
+                    [{text: '/start'}],
+                ]
+            }
+        })
+    }
+});
